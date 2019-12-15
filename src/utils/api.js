596b7a283c1fd97a5048f2c63745eefeb6f6943f
Benjamim Alves <benjamim.multimedia@gmail.com>
2019-12-15 18:34:38 +0000

    changed cors service for my own domain

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/prefer-default-export
-export const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
+export const CORS_ANYWHERE = 'https://benjamimalves.com/cors-anywhere.php?url=';
 export const CARRIS_API = `${CORS_ANYWHERE}https://carris.tecmic.com/api/v2.6/`;
 
 export const ENDPOINTS = {
   GEOCODING: {
     SUGGEST: `${CARRIS_API}Geocoding/suggest/`,
-    FROMID: `${CARRIS_API}Geocoding/fromId/`,
+    FROMID: `${CARRIS_API}Geocoding/fromId/`
   },
   PLANNER: `${CARRIS_API}Planner/startlat/:startlat/startlon/:startlon/endLat/:endLat/endLon/:endLon/start/:datetime/language/en`
 };
